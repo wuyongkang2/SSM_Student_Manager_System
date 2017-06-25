48f6efde8be6a110494b7e3b002d67a560c52c77
伍永康 <2014561112@qq.com>
2017-06-25 17:17:40 +0800

    班级增加模块（数据层）

--- docs/13class_add（data）.md
+++ docs/13class_add（data）.md
@@ -2,3 +2,5 @@
 
 1. 定义vo类，名称为Classes.java
 	![](../images/34.jpg)  
+2. 定义dao接口并继承IDAO.java接口  
+	![](../images/35.jpg)  
---
+++ studentpro/src/main/java/cn/ylcto/student/dao/impl/IClassesDAO.java
@@ -0,0 +1,10 @@
+package cn.ylcto.student.dao.impl;
+
+import cn.ylcto.student.dao.IDAO;
+import cn.ylcto.student.vo.Classes;
+
+/**
+ * Created by kangkang on 2017/6/25.
+ */
+public interface IClassesDAO extends IDAO<Integer,Classes> {
+}
