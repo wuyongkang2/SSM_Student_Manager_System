3a6469139bdb805e9a45605e6344d57d5fa49983
伍永康 <2014561112@qq.com>
2017-06-30 11:39:00 +0800

    学生列表（数据层实现）

--- studentpro/src/main/java/cn/ylcto/student/dao/IStudentDAO.java
+++ studentpro/src/main/java/cn/ylcto/student/dao/IStudentDAO.java
@@ -3,8 +3,26 @@ package cn.ylcto.student.dao;
 import cn.ylcto.student.dao.IDAO;
 import cn.ylcto.student.vo.Student;
 
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Created by kangkang on 2017/6/29.
  */
 public interface IStudentDAO extends IDAO<String,Student> {
+    /**
+     * 实现数据分页操作
+     * @param currentPage 表示当前页
+     * @param lineSize 表示每页显示记录数
+     * @return 成功返回满足条件的数据，失败返回null
+     * @throws SQLException
+     */
+    public List<Student> findAllBySplit(Integer currentPage, Integer lineSize)throws SQLException;
+
+    /**
+     * 实现数据量统计操作
+     * @return 成功返回数据量，失败返回 0
+     * @throws SQLException
+     */
+    public Integer getAllCount()throws SQLException;
 }
